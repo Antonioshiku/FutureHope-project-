eb0cab6a7f1506551e5b6eb4e144cb67218e619f
joekudo <joekudo303@gmail.com>
2022-06-09 10:25:59 +0630

    change form register and log in

--- js/form.js
+++ js/form.js
@@ -1,88 +1,71 @@
-let Form = document.querySelector("#Log");
-let Reg = document.querySelector("#Reg");
-let Button=document.querySelector('#Log_button');
-let regButton=document.querySelector('#Reg_button');
-let RegText=document.querySelector('#Reg_text');
+// let Form = document.querySelector("#Log");
+// let Reg = document.querySelector("#Reg");
+// let Button = document.querySelector("#Log_button");
+// let regButton = document.querySelector("#Reg_button");
+// let RegText = document.querySelector("#Reg_text");
 
+// Form.addEventListener("click", () => {
+//   LogInForm();
+// });
 
+// Reg.addEventListener("click", () => {
+//   RegistentionForm();
+// });
 
-Form.addEventListener("click", () => {
+// function LogInForm() {
+//   if (Form.innerHTML == "Login") {
+//     let paForm = Form.parentElement.parentElement.parentElement;
+//     paForm.classList.add("d-none");
+//     let paReg = Reg.parentElement.parentElement.parentElement;
+//     paReg.classList.remove("d-none");
+//   } else {
+//     let paForm = Form.parentElement.parentElement.parentElement;
+//     paForm.classList.remove("d-none");
+//     let paReg = Reg.parentElement.parentElement.parentElement;
+//     paReg.classList.add("d-none");
+//   }
 
-  LogInForm();
-});
+//   let RegisterForm = Button.parentElement;
+//   RegisterForm.classList.add("d-none");
+//   let logInButton = regButton.parentElement;
+//   logInButton.classList.remove("d-none");
 
-Reg.addEventListener("click", () => {
-
-  RegistentionForm();
-});
-
-
-
-
-function LogInForm() {
-  if (Form.innerHTML == "Login") {
-    let paForm = Form.parentElement.parentElement.parentElement;
-    paForm.classList.add("d-none");
-    let paReg = Reg.parentElement.parentElement.parentElement;
-    paReg.classList.remove("d-none");
-  } else {
-    let paForm = Form.parentElement.parentElement.parentElement;
-    paForm.classList.remove("d-none");
-    let paReg = Reg.parentElement.parentElement.parentElement;
-    paReg.classList.add("d-none");
-  }
-
-
-       
-       let RegisterForm=Button.parentElement;
-       RegisterForm.classList.add('d-none');
-       let logInButton=regButton.parentElement;
-       logInButton.classList.remove('d-none');
-       
-       RegText.innerHTML="Sign Up Here";
-       RegText.style.transform='translateY(40px)';
-
-
-
-}
-
-
-
-
-function RegistentionForm() {
-  if (Form.innerHTML == "Register") {
-    let paForm = Form.parentElement.parentElement.parentElement;
-    paForm.classList.add("d-none");
-    let paReg = Reg.parentElement.parentElement.parentElement;
-    paReg.classList.remove("d-none");
-  } else {
-    let paForm = Form.parentElement.parentElement.parentElement;
-    paForm.classList.remove("d-none");
-    let paReg = Reg.parentElement.parentElement.parentElement;
-    paReg.classList.add("d-none");
-  }
-
-       
-    let RegisterForm=Button.parentElement;
-    RegisterForm.classList.remove('d-none');
-    let logInButton=regButton.parentElement;
-    logInButton.classList.add('d-none');
-    
-
-    RegText.innerHTML="Register Here";
-    RegText.style.transform='translateY(0px)';
-
-
-}
-
-
-
-// if  (window.screen.width>818 && window.screen.width<1100) {
-  
-//     let formMain=document.querySelector('.main_con');
-//     formMain.classList.add('show_main_form')
-
-
-// } else {
-//     console.log("error");
+//   RegText.innerHTML = "Sign Up Here";
+//   RegText.style.transform = "translateY(40px)";
 // }
+
+// function RegistentionForm() {
+//   if (Form.innerHTML == "Register") {
+//     let paForm = Form.parentElement.parentElement.parentElement;
+//     paForm.classList.add("d-none");
+//     let paReg = Reg.parentElement.parentElement.parentElement;
+//     paReg.classList.remove("d-none");
+//   } else {
+//     let paForm = Form.parentElement.parentElement.parentElement;
+//     paForm.classList.remove("d-none");
+//     let paReg = Reg.parentElement.parentElement.parentElement;
+//     paReg.classList.add("d-none");
+//   }
+
+//   let RegisterForm = Button.parentElement;
+//   RegisterForm.classList.remove("d-none");
+//   let logInButton = regButton.parentElement;
+//   logInButton.classList.add("d-none");
+
+//   RegText.innerHTML = "Register Here";
+//   RegText.style.transform = "translateY(0px)";
+// }
+
+// window.addEventListener('resize', () => {
+
+//   let formMain=document.querySelector('.main_con');
+
+//   if (window.innerWidth>809 && window.innerWidth<1200) {
+//     formMain.classList.add('show_main_form');
+//   } else {
+//     formMain.classList.remove('show_main_form');
+    
+//   }
+// })
+
+
